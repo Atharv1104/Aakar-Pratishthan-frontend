89a87d0a952c46817b1ed3e1930d3bcfbdfa502d
Atharv1104 <atharvpore2004@gmail.com>
2025-12-05 22:49:14 +0530

    fixed contact form

--- src/components/Forms/ContactForm.jsx
+++ src/components/Forms/ContactForm.jsx
@@ -27,7 +27,7 @@ function RegForm() {
 
     const onSubmit = async (data) => {
         try {
-            const res = await apiClient("/api/contact", {
+            const res = await apiClient("/contact", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(data),
